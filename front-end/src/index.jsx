513ed3ed8ff608715904e09dc3585c02aec7a358
Marcio JF Santos <mjose@ltm.digital>
2019-02-20 16:17:03 -0300

    first commit

---
+++ front-end/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './img/favicon.ico'
+
+import './css/index.css'
+import './css/pure-min.css'
+
+
+ReactDOM.render(<h1>Bem-vindo ao react</h1>, document.querySelector('#main'))
---
+++ front-end/webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.jsx'),
+    output:{
+        path: path.join(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:path.join(__dirname,'src/index.html')
+        })
+    ],
+    module:{
+        rules:
+            [
+                {
+                    test:/.jsx?$/,
+                    exclude:/node_modules/,
+                    include:path.join(__dirname,'src'),
+                    use:[
+                        {
+                            loader:'babel-loader',
+                            options:{
+                                presets:['es2015','react']
+                            }
+                        }
+                    ]
+                },
+                {
+                    test:/\.(jpe?g|ico|png|gif|svg)$/i,
+                    loader:'file-loader?name=img/[name].[ext]'
+
+                },
+                {
+                    test:/.css$/,
+                    use:[
+                        {loader:'style-loader'},
+                        {loader:'css-loader'}
+                    ]
+                }
+            ]
+        
+    },
+    devServer:{
+        publicPath:'/',
+        contentBase:'./dist'
+    },
+    resolve:{
+        extensions:[".js",".jsx"]
+    }
+}
